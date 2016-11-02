931e27a90842a8b1d9314a71dc1f0184ee3dfd49
Hajime Tazaki <thehajime@gmail.com>
2016-11-02 17:04:57 +0900

    Revert "Revert "undefine mmap/munmap/mprotect""
    This reverts commit 9998747893d3ea63da0983dec11b32eddff339a0.

--- arch/lkl/src/mmap.c
+++
@@ -1,7 +0,0 @@
-#include <sys/mman.h>
-
-void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
-{
-	/* call franken's mmap stub */
-	return mmap(start, len, prot, flags, fd, off);
-}
--- arch/lkl/src/munmap.c
+++
@@ -1,7 +0,0 @@
-#include <sys/mman.h>
-
-int __munmap(void *start, size_t len)
-{
-	/* call franken's munmap stub */
-	return munmap(start, len);
-}
