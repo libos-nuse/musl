4d49d8209d4a40175c56172e2c992c4d3469b4ae
Hajime Tazaki <thehajime@gmail.com>
2017-03-14 21:41:59 +0900

    Revert "lkl: more proper pthread_self/clone support with rump hypcall"
    This reverts commit 585ffff0c2bf0db5b007607b75e926a0d6f4f89f.

--- arch/lkl/src/clone.c
+++ arch/lkl/src/clone.c
@@ -2,7 +2,6 @@
 
 int  rumpuser_thread_create(void *(*f)(void *), void *, const char *, int,
 			    int, int, void **);
-void rumpuser_thread_set_cookie(void *thread, void *cookie);
 
 int __clone(int (*func)(void *), void *stack, int flags, void *arg, ...)
 {
@@ -14,6 +13,5 @@ int __clone(int (*func)(void *), void *stack, int flags, void *arg, ...)
 				     "__clone", 0, 0, -1, &tid);
 
 	self->tid = *(int *)tid;
-	rumpuser_thread_set_cookie(tid, self);
 	return ret;
 }
